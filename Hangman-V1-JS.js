524eabf200cd7cb12811ae8fa1ead03e440ab433
TiffanyH <tiffanyxho@gmail.com>
2018-06-18 18:30:00 -0700

    added documentation

--- Hangman-V1-JS.js
+++ Hangman-V1-JS.js
@@ -1,16 +1,28 @@
 // initialize empty list called wordList
-//var wordsList = [];
+var wordsList = ['wooooo'];
+var tester;
+
+function guessWord(){
+    var text = document.getElementById('guessWord').value;
+    console.log(text);
+}
+
+window.onload = function main (){
+    init();
+    test = init();
+    console.log(wordsList);
+    tester = init();
+    console.log(tester);
+}
 
 // Load the words from the dictionary text file to wordsList
 function init() {
+    console.log('hello from init im gonna kill JS I HATE JS');
     // words in test file: test, testing, tester, tests
     var wordsFile = "https://raw.githubusercontent.com/tiffanyxho/Hangman-V1/master/Hangman-V1-TestWordsList.txt";
     //var wordList;
     $.get(wordsFile, function(data) {
         var wordList = data.split('\n');
-        wordList.forEach(function(element){
-            console.log(element);
-        });
         return wordList;
     });
     // alternate option for top...  not sure which works better
@@ -25,12 +37,19 @@ function init() {
     });*/
 }
 
-function guessWord(){
-    var text = document.getElementById('guessWord').value;
-    console.log(text);
-}
+/*
+var wordsFile = "https://raw.githubusercontent.com/tiffanyxho/Hangman-V1/master/Hangman-V1-TestWordsList.txt";
+var wordList;
+$.get(wordsFile, function(data) {
+    wordList = data.split('\n');
+    wordList.forEach(function(element){
+        console.log(element);
+    });
+    console.log(wordList);
+});
 
-window.onload = function main (){
-    var wordsList = init();
-    console.log(wordsList);
-}
+
+// for some reason this prints undefined when i have !==, but doesn't print undefined when i have ===
+if (typeof (wordsList) === 'undefined'){
+    console.log(wordList);
+}*/
