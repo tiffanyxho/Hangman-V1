d63b73b3c8d85796cb4cf3879e04997e9cdce518
TiffanyH <tiffanyxho@gmail.com>
2018-06-20 14:31:15 -0700

    added new JS file for hangman V1.1 where player can guess 1 letter at a time like actual hangman instead of entire word; also added ideas to hangman V1 JS file for future versions commented out at top

--- Hangman-V1.1-JS.js
+++ Hangman-V1.1-JS.js
@@ -1,15 +1,13 @@
 // AFTER: ALLOW USER TO GUESS 1 LETTER AT A TIME
-// AFTER: ALLOW USER TO GUESS MULTIPLE LETTERS AT A TIME - COUNTS AS ONLY 1 "ERROR"
 // DECIDE ON A SCORING SYSTEM (HOW DOES THE PLAYER LOSE?)
-// different difficulty modes - easy, medium, hard; multiple array lists with levels
-// different themes: cat & mouse - mouse running --> powerups, boots to make run faster
-// theme 2: car & gas; refill gas, more gas efficient = more turns
+
 
 // declare/initialize global variables
 let wordsList = newArr();
 let randWord = '';
 let randNum;
 let correctGuesses = 0;
+let guessesLeft = 10;
 
 // checks when user pushes enter key & also checks if you guessed the right word, displays whether you got right word or not
 var checkEnter = window.addEventListener('keypress', function (e) {
@@ -59,17 +57,6 @@ function guessWord(){
     return text;
 }
 
-// main function - TEST #2
-/*window.onload = function main(){
-    // store text file with list of words in wordsFile
-    var wordsFile = "https://raw.githubusercontent.com/tiffanyxho/Hangman-V1/master/Hangman-V1-TestWordsList.txt";
-
-    // separate words in wordList into strings and store in array using split
-    $.get(wordsFile, function(data) {
-        var wordList = data.split('\n');
-    });
-}*/
-
 // checks if randomWord == userGuess & log result & return value: true/right or false/wrong
 function checkGuess(randomWord, userGuess){
     if (randomWord == userGuess){
@@ -80,26 +67,6 @@ function checkGuess(randomWord, userGuess){
         return false;
     }
 }
-
-/*
-var wordList = [];
-// Load the words from the dictionary text file to wordsList
-window.onload = function init() {
-    console.log('from init O:<');
-    // words in test file: test, testing, tester, tests
-    var wordsFile = "https://raw.githubusercontent.com/tiffanyxho/Hangman-V1/master/Hangman-V1-TestWordsList.txt";
-    $.get(wordsFile, function(data) {
-        //var wordList = data.split('\n');
-        wordList = data.split('\n');
-        //return wordList;
-        
-    });
-    console.log(wordList);
-    if (wordList.length === 0){
-        console.log('im empty');
-    }
-}
-*/
 
 function newArr(){
     return ['rest', 'beast', 'cat', 'dude'];
